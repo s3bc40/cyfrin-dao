6db2dc08e1aed17d63eb687345335fb225e1f951
s3bc40 <s3bc40@gmail.com>
2024-11-10 10:41:25 +0100

    feat: finished test dao and governance example contract

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -6,9 +6,13 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20, ERC20Permit, ERC20Votes {
+contract GovToken is ERC20, ERC20Permit, ERC20Votes {
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
 
+    // Public for testing purposes
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
     // The following functions are overrides required by Solidity.
 
     function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18
+pragma solidity ^0.8.18;
 
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
-contract TimeLock is TimeLockController{
-    constructor(uint256 minDelay, address[] memory proposers, address memory executors) TimelockController(minDelay, proposers, executors, msg.sender) {}
+contract TimeLock is TimelockController{
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors) TimelockController(minDelay, proposers, executors, msg.sender) {}
 }
